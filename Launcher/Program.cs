210cbdc28dee065fa8d5a584f3b70538caeca65c
Reed Rector <rrector@microsoft.com>
2020-06-27 16:38:56 -0700

    Initial version

---
+++ Launcher/Program.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Providers;
+using Orleans.Providers.Streams.Common;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Launcher
+{
+    class Program
+    {
+        public static Guid streamGuid = Guid.NewGuid();
+
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var host = await StartSilo();
+                var client = await CreateClient();
+
+                await Program.ProduceEvents(client);
+
+                await host.StopAsync();
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            var builder = new SiloHostBuilder()
+                // Use localhost clustering for a single local silo
+                .UseLocalhostClustering()
+                // Configure ClusterId and ServiceId
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "MyAwesomeService";
+                })
+            // Configure connectivity
+            .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                // Configure logging with any logging framework that supports Microsoft.Extensions.Logging.
+                // In this particular case it logs using the Microsoft.Extensions.Logging.Console package.
+                .ConfigureLogging(logging => logging.AddConsole())
+            .AddMemoryStreams<DefaultMemoryMessageBodySerializer>("MemoryTest")
+            .AddMemoryGrainStorage("PubSubStore")
+            .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IReceiverGrain).Assembly).WithCodeGeneration())
+            ;
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
+        }
+
+        private static async Task<IClusterClient> CreateClient()
+        {
+            var client = new ClientBuilder()
+                // Clustering information
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "MyAwesomeClient";
+                })
+                // Clustering provider
+                .UseLocalhostClustering()
+                .AddMemoryStreams<DefaultMemoryMessageBodySerializer>("MemoryTest")
+
+                // Application parts: just reference one of the grain interfaces that we use
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IReceiverGrain).Assembly).WithCodeGeneration())
+                .Build();
+
+            await client.Connect();
+            return client;
+        }
+
+        private static async Task ProduceEvents(IClusterClient client)
+        {
+            var ourSequence = 0;
+            var rng = new Random();
+            var streamProvider = client.GetStreamProvider("MemoryTest");
+            var stream = streamProvider.GetStream<Tuple<int, int>>(streamGuid, "TESTDATA");
+
+            while (true)
+            {
+                Console.WriteLine("Enter the number of events to generate, 0 to quit");
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out var numEvents))
+                {
+                    if (numEvents < 1)
+                    {
+                        return;
+                    }
+
+                    var curEvent = 0;
+
+                    while (curEvent < numEvents)
+                    {
+                        var batchSize = Math.Min(numEvents - curEvent, rng.Next(1, 5));
+                        var batch = new List<Tuple<int, int>>(batchSize);
+
+                        for (int i = 0; i < batchSize; i++, curEvent++, ourSequence++)
+                        {
+                            batch.Add(new Tuple<int, int>(ourSequence, i));
+                        }
+
+                        await stream.OnNextBatchAsync(batch);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Launcher/ReceiverGrain.cs
@@ -0,0 +1,55 @@
+﻿using Orleans;
+using Orleans.Streams;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher
+{
+    public interface IReceiverGrain : IGrainWithGuidKey
+    {
+        
+    }
+
+    public class MyObserver : IAsyncObserver<Tuple<int, int>>
+    {
+        public Task OnCompletedAsync()
+        {
+            Console.WriteLine("Hi OnCompletedAsync");
+            return Task.CompletedTask;
+        }
+
+        public Task OnErrorAsync(Exception ex)
+        {
+            Console.WriteLine($"In OnErrorAsync: {ex}");
+            return Task.CompletedTask;
+        }
+
+        public Task OnNextAsync(Tuple<int, int> item, StreamSequenceToken token = null)
+        {
+            Console.WriteLine($"Received item, Our sequence is {item.Item1} and batch is {item.Item2}. Sequence Token is (SN:{token?.SequenceNumber}, EI:{token?.EventIndex})");
+            return Task.CompletedTask;
+        }
+    }
+
+    [ImplicitStreamSubscription("TESTDATA")]
+    public class ReceiverGrain : Grain, IReceiverGrain
+    {
+        public override async Task OnActivateAsync()
+        {
+            //Create a GUID based on our GUID as a grain
+            var guid = this.GetPrimaryKey();
+            //Get one of the providers which we defined in config
+            var streamProvider = GetStreamProvider("MemoryTest");
+            //Get the reference to a stream
+            var stream = streamProvider.GetStream<Tuple<int, int>>(guid, "TESTDATA");
+
+            //Set our OnNext method to the lambda which simply prints the data, this doesn't make new subscriptions because we are using implicit subscriptions via [ImplicitStreamSubscription].
+
+            await stream.SubscribeAsync<Tuple<int, int>>(async (item, token) => Console.WriteLine($"Received item, Our sequence is {item.Item1} and batch is {item.Item2}. Sequence Token is (SN:{token?.SequenceNumber}, EI:{token?.EventIndex})"));
+
+            await base.OnActivateAsync();
+        }
+    }
+}
